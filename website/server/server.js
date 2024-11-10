7e3d871e27cb25517e1dde6d478a0ca12e8b7a83
mostafa-abokhadra <mvxizk@gmail.com>
2024-11-10 03:50:01 +0200

    nav bar initialized

--- website/server/server.js
+++ website/server/server.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const path = require('path');
 
 app.set("view engine", 'ejs')
-
+app.use(express.static(path.join(__dirname, 'public')));
 const homeRoute = require("./routes/homoRoute") 
 
 app.use('/', homeRoute)
